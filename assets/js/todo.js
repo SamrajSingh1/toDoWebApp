5cf1753ad84d5a1ef23fb4988fa1148724f72a14
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2018-03-12 19:46:57 +1100

    added delete functionality to span elements in todo.js

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -2,3 +2,13 @@
 $('li').on('click', function() {
   $(this).toggleClass('completed');
 });
+
+// Click on X to delete todo
+$('span').on('click', function(event) {
+  $(this)
+    .parent()
+    .fadeOut(500, function() {
+      $(this).remove();
+    });
+  event.stopPropagation();
+});
